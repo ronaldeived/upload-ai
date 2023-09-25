90f736fe615331f093ef2a2a18379bccddc99e3c
Ronaldeived Silva <rona_dev@outlook.com>
2023-09-25 18:52:45 -0300

    creating the api and routes

---
+++ upload-ai-api/src/server.ts
@@ -0,0 +1,23 @@
+import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors';
+import { getAllPrompts } from "./routes/get-all-prompts";
+import { uploadVideo } from "./routes/upload-video";
+import { createTranscription } from "./routes/create-transcription";
+import { generationAICompletion } from "./routes/generate-ai-completion";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+    origin: '*'
+});
+
+app.register(getAllPrompts);
+app.register(uploadVideo);
+app.register(createTranscription);
+app.register(generationAICompletion);
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP Server Running');
+});
